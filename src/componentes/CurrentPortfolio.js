7332d5393880357a11ef29302a889e5800f94ae3
Lenin Montalvo <lenin.montalvo@oktana.com>
2021-10-25 10:20:16 -0500

    Add form

--- src/componentes/CurrentPortfolio.js
+++ src/componentes/CurrentPortfolio.js
@@ -1,4 +1,28 @@
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 export default function CurrentPortfolio(props) {
- return <h2>Please Enter Your Current Portfolio</h2>;
+ return (
+  <div>
+   <h2>Please Enter Your Current Portfolio</h2>
+   <Form>
+    <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+     <Form.Label column sm={2}>
+      Email
+    </Form.Label>
+     <Col sm={10}>
+      <Form.Control type="email" placeholder="Email" />
+     </Col>
+    </Form.Group>
+
+    <Form.Group as={Row} className="mb-3">
+     <Col sm={{ span: 10, offset: 2 }}>
+      <Button type="submit">Sign in</Button>
+     </Col>
+    </Form.Group>
+   </Form>
+  </div>
+ );
 }
